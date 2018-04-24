7317303f9ecdfa851ecdd017048b3f2b47f3cad4
nuanbing <jinghui@wayne.edu>
2018-04-24 20:41:06 +0800

    修改nnc转账bug

--- smartContractDemo/tests/nnc.cs
+++ smartContractDemo/tests/nnc.cs
@@ -16,24 +16,41 @@ namespace smartContractDemo
         byte[] scripthash;
         byte[] pubkey;
         Hash160 reg_sc;//注册器合约地址
-        public string[] LIST = {
-            "exit",
-            "totalSupply",
-            "name",
-            "symbol",
-            "decimals",
-            "balanceOf",
-            "transfer",
-            "transfer_app",
-            "deploy",
-            "balanceOfDetail",
-            "use",
-            "use_app",
-            "getTXInfo",
-            "getBonus",
-            "checkBonus",
-            "newBonus"
-        };
+        public delegate Task testAction();
+        public Dictionary<string, testAction> infos = null;// = new Dictionary<string, testAction>();
+        string[] submenu;
+
+        void subPrintLine(string line)
+        {
+            Console.WriteLine("    " + line);
+        }
+
+        public nnc()
+        {
+            this.initManu();
+        }
+
+        private void initManu()
+        {
+            infos = new Dictionary<string, testAction>();
+
+            infos["totalSupply"] = test_not_implement_yet;
+            infos["name"] = test_not_implement_yet;
+            infos["symbol"] = test_not_implement_yet;
+            infos["decimals"] = test_not_implement_yet;
+            infos["balanceOf"] = test_BalanceOf;
+            infos["transfer"] = test_Transfer;
+            infos["transfer_app"] = test_not_implement_yet;
+            infos["deploy"] = test_not_implement_yet;
+            infos["balanceOfDetail"] = test_not_implement_yet;
+            infos["use"] = test_not_implement_yet;
+            infos["use_app"] = test_not_implement_yet;
+            infos["getTXInfo"] = test_not_implement_yet;
+            infos["getBonus"] = test_not_implement_yet;
+            infos["checkBonus"] = test_CheckBonus;
+            infos["newBonus"] = test_NewBonus;
+            this.submenu = new List<string>(infos.Keys).ToArray();
+        }
 
         public delegate void Method(ThinNeo.ScriptBuilder sb);//第一步：定义委托类型
 
@@ -44,274 +61,127 @@ namespace smartContractDemo
             this.reg_sc = new Hash160(info_reg.value.subItem[0].subItem[1].data);
             Console.WriteLine("reg=" + reg_sc.ToString());
 
-            for (int i = 0; i < LIST.Length; i++)
-            {
-                Console.WriteLine(i + ":" + LIST[i]);
-            }
-            Boolean run = true;
-            while (run)
-            {
-                var line = Convert.ToInt16(Console.ReadLine());
+            showMenu();
 
-                if (line < 0 || line >= LIST.Length)
+            prikey = ThinNeo.Helper.GetPrivateKeyFromWIF(nnc_1.testwif);
+            pubkey = ThinNeo.Helper.GetPublicKeyFromPrivateKey(prikey);
+            address = ThinNeo.Helper.GetAddressFromPublicKey(pubkey);
+            scripthash = ThinNeo.Helper.GetPublicKeyHashFromAddress(address);
+
+            while (true)
+            {
+                var line = Console.ReadLine().Replace(" ", "").ToLower();
+                if (line == "?" || line == "？")
                 {
-                    Console.WriteLine("参数错误！");
+                    showMenu();
+                }
+                else if (line == "")
+                {
                     continue;
                 }
-                else if (LIST[line] == "exit")
+                else if (line == "0")
                 {
-                    run = false;
-                    Console.WriteLine("已退出 NNC 测试");
-                    continue;
+                    return;
                 }
-
-                Console.WriteLine("[begin]" + ID + " : " + LIST[line]);
-
-                prikey = ThinNeo.Helper.GetPrivateKeyFromWIF(nnc_1.testwif);
-                pubkey = ThinNeo.Helper.GetPublicKeyFromPrivateKey(prikey);
-                address = ThinNeo.Helper.GetAddressFromPublicKey(pubkey);
-                scripthash = ThinNeo.Helper.GetPublicKeyHashFromAddress(address);
-
-                Console.WriteLine("address=" + address);
-                string script = null;
-                byte[] postdata;
-                try
+                else//get .test's info
                 {
-                    switch (LIST[line])
+                    var id = int.Parse(line) - 1;
+                    var key = submenu[id];
+                    subPrintLine("[begin]" + key);
+                    try
                     {
-                        case "totalSupply":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "name":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "symbol":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "decimals":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "balanceOf":
-                            Invoke(new Method(BalanceOf));
-                            break;
-                        case "transfer":
-                            Transac(new Method(Transfer));
-                            break;
-                        case "transfer_app":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "deploy":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "balanceOfDetail":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "use":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "use_app":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "getTXInfo":
-                            Console.WriteLine("尚未实现");
-                            continue;
-                            break;
-                        case "getBonus":
-                            Transac(new Method(GetBonus));
-                            break;
-                        case "checkBonus":
-                            Invoke(new Method(CheckBonus));
-                            break;
-                        case "newBonus":
-                            Invoke(new Method(NewBonus));
-                            break;
+                        await infos[key]();
                     }
+                    catch (Exception err)
+                    {
+                        subPrintLine(err.Message);
+                    }
+                    subPrintLine("[end]" + key);
                 }
-                catch (Exception e)
+            }
+        }
+
+        void showMenu()
+        {
+            for (var i = 0; i < submenu.Length; i++)
+            {
+                var key = submenu[i];
+                subPrintLine((i + 1).ToString() + ":" + key);
+            }
+            subPrintLine("0:exit");
+            subPrintLine("?:show this");
+        }
+
+        async Task test_not_implement_yet()
+        {
+            subPrintLine("尚未实现");
+        }
+
+        async Task test_BalanceOf()
+        {
+            Console.WriteLine("Input target address (" + this.address + "):");
+            string addr;
+            try
+            {
+                addr = Console.ReadLine();
+                if (addr == "\n")
                 {
-                    Console.WriteLine(e);
+                    addr = this.address;
                 }
-
-                Console.WriteLine("[end]" + LIST[line]);
             }
-        }
-
-        private async void Transac(Method method)
-        {
-            //获取地址的资产列表
-            Dictionary<string, List<Utxo>> dir = await Helper.GetBalanceByAddress(Nep55_1.api, address);
-            if (dir.ContainsKey(Nep55_1.id_GAS) == false)
+            catch (Exception e)
             {
-                Console.WriteLine("no gas");
-                return;
+                addr = this.address;
             }
 
-            //MakeTran
-            ThinNeo.Transaction tran = null;
-            {
-                byte[] script = Builder(method);
-                tran = Helper.makeTran(dir[Nep55_1.id_GAS], address, new ThinNeo.Hash256(Nep55_1.id_GAS), 0);
-                tran.type = ThinNeo.TransactionType.InvocationTransaction;
-                var idata = new ThinNeo.InvokeTransData();
-                tran.extdata = idata;
-                idata.script = script;
-            }
-            //sign and broadcast
-            var signdata = ThinNeo.Helper.Sign(tran.GetMessage(), prikey);
-            tran.AddWitness(signdata, pubkey, address);
-            var trandata = tran.GetRawData();
-            var strtrandata = ThinNeo.Helper.Bytes2HexString(trandata);
-            byte[] postdata;
-            var url = Helper.MakeRpcUrlPost(Nep55_1.api, "sendrawtransaction", out postdata, new MyJson.JsonNode_ValueString(strtrandata));
-            var result = await Helper.HttpPost(url, postdata);
-            Console.WriteLine("得到的结果是：" + result);
-        }
+            byte[] hash = ThinNeo.Helper.GetPublicKeyHashFromAddress(addr);
+            string strhash = ThinNeo.Helper.Bytes2HexString(hash);
 
-        private async void Invoke(Method method)
-        {
-            byte[] data = Builder(method);
-            string script = ThinNeo.Helper.Bytes2HexString(data);
-
-            byte[] postdata;
-            var url = Helper.MakeRpcUrlPost(nnc_1.api, "invokescript", out postdata, new MyJson.JsonNode_ValueString(script));
-            var result = await Helper.HttpPost(url, postdata);
-            Console.WriteLine("得到的结果是：" + result);
-        }
-
-        private byte[] Builder(Method method)
-        {
-            byte[] data = null;
-            using (var sb = new ThinNeo.ScriptBuilder())
-            {
-                method(sb);
-                data = sb.ToArray();
-
-            }
-            return data;
-        }
-
-        private void BalanceOf(ThinNeo.ScriptBuilder sb)
-        {
-            var array = new MyJson.JsonNode_Array();
-            array.AddArrayValue("(bytes)" + ThinNeo.Helper.Bytes2HexString(scripthash));
-            sb.EmitParamJson(array);//参数倒序入
-            sb.EmitParamJson(new MyJson.JsonNode_ValueString("(str)balanceOf"));//参数倒序入
             ThinNeo.Hash160 shash = new ThinNeo.Hash160(nnc_1.sc_nnc);
-            sb.EmitAppCall(shash);//nep5脚本
-
-            sb.EmitParamJson(array);//参数倒序入
-            sb.EmitParamJson(new MyJson.JsonNode_ValueString("(str)balanceOf"));//参数倒序入
-            sb.EmitAppCall(reg_sc);//nep5脚本
+            var result = await nns_common.api_InvokeScript(shash, "balanceOf", "(bytes)" + strhash);
+            //subPrintLine(result);
         }
 
-        private void NewBonus(ThinNeo.ScriptBuilder sb)
+        async Task test_NewBonus()
         {
-            var array = new MyJson.JsonNode_Array();
-            array.AddArrayValue("(bytes)" + ThinNeo.Helper.Bytes2HexString(scripthash));
-            sb.EmitParamJson(array);//参数倒序入
-            sb.EmitParamJson(new MyJson.JsonNode_ValueString("(str)newBonus"));//参数倒序入
-            ThinNeo.Hash160 shash = new ThinNeo.Hash160(nnc_1.sc_nnc);
-            sb.EmitAppCall(shash);//nep5脚本
+            //var array = new MyJson.JsonNode_Array();
+            //array.AddArrayValue("(bytes)" + ThinNeo.Helper.Bytes2HexString(scripthash));
+            //sb.EmitParamJson(array);//参数倒序入
+            //sb.EmitParamJson(new MyJson.JsonNode_ValueString("(str)newBonus"));//参数倒序入
+            //ThinNeo.Hash160 shash = new ThinNeo.Hash160(nnc_1.sc_nnc);
+            //sb.EmitAppCall(shash);//nep5脚本
         }
 
-        private void GetBonus(ThinNeo.ScriptBuilder sb)
+        async Task test_GetBonus(ThinNeo.ScriptBuilder sb)
         {
-            var array = new MyJson.JsonNode_Array();
-            array.AddArrayValue("(bytes)" + ThinNeo.Helper.Bytes2HexString(scripthash));
-            sb.EmitParamJson(array);//参数倒序入
-            sb.EmitParamJson(new MyJson.JsonNode_ValueString("(str)getBonus"));//参数倒序入
             ThinNeo.Hash160 shash = new ThinNeo.Hash160(nnc_1.sc_nnc);
-            sb.EmitAppCall(shash);//nep5脚本
 
-            sb.EmitSysCall("System.ExecutionEngine.GetScriptContainer");
-            sb.EmitSysCall("Neo.Transaction.GetHash");
-            //把TXID包进Array里
-            sb.EmitPushNumber(1);
-            sb.Emit(ThinNeo.VM.OpCode.PACK);
-            sb.EmitPushString("setmoneyin");
-            sb.EmitAppCall(reg_sc);
+            var result = await nns_common.api_SendTransaction(shash, prikey, "getBonus", "(bytes)" + ThinNeo.Helper.Bytes2HexString(scripthash));
+            subPrintLine(result);
         }
 
-        private void CheckBonus(ThinNeo.ScriptBuilder sb)
+        async Task test_CheckBonus()
         {
-            var array = new MyJson.JsonNode_Array();
-            array.AddArrayValue("(bytes)" + ThinNeo.Helper.Bytes2HexString(scripthash));
-            sb.EmitParamJson(array);//参数倒序入
-            sb.EmitParamJson(new MyJson.JsonNode_ValueString("(str)checkBonus"));//参数倒序入
             ThinNeo.Hash160 shash = new ThinNeo.Hash160(nnc_1.sc_nnc);
-            sb.EmitAppCall(shash);//nep5脚本
+            var result = await nns_common.api_InvokeScript(shash, "checkBonus", "(bytes)" + ThinNeo.Helper.Bytes2HexString(scripthash));
+            //subPrintLine(result);
         }
 
-        private void Transfer(ThinNeo.ScriptBuilder sb)
+        async Task test_Transfer()
         {
             Console.WriteLine("Input target address:");
             string addressto = Console.ReadLine();
             Console.WriteLine("Input amount:");
             string amount = Console.ReadLine();
+
             ThinNeo.Hash160 shash = new ThinNeo.Hash160(nnc_1.sc_nnc);
-             this.api_SendTransaction(prikey, shash, "(str)transfer",
-              "(int)"+ amount,
+
+            var result = await nns_common.api_SendTransaction(prikey, shash, "transfer",
+              "(int)" + amount,
               "(addr)" + addressto,
               "(addr)" + address
               );
-        }
-
-        public async Task<string> api_SendTransaction(byte[] prikey, Hash160 schash, string methodname, params string[] subparam)
-        {
-            byte[] pubkey = ThinNeo.Helper.GetPublicKeyFromPrivateKey(prikey);
-            string address = ThinNeo.Helper.GetAddressFromPublicKey(pubkey);
-
-            //获取地址的资产列表
-            Dictionary<string, List<Utxo>> dir = await Helper.GetBalanceByAddress(nnc_1.api, address);
-            if (dir.ContainsKey(Nep55_1.id_GAS) == false)
-            {
-                Console.WriteLine("no gas");
-                return null;
-            }
-            //MakeTran
-            ThinNeo.Transaction tran = null;
-            {
-
-                byte[] data = null;
-                using (ScriptBuilder sb = new ScriptBuilder())
-                {
-                    MyJson.JsonNode_Array array = new MyJson.JsonNode_Array();
-                    for (var i = 0; i < subparam.Length; i++)
-                    {
-                        array.AddArrayValue(subparam[i]);
-                    }
-                    sb.EmitParamJson(array);
-                    sb.EmitPushString(methodname);
-                    sb.EmitAppCall(schash);
-                    data = sb.ToArray();
-                }
-
-                tran = Helper.makeTran(dir[Nep55_1.id_GAS], null, new ThinNeo.Hash256(Nep55_1.id_GAS), 0);
-                tran.type = ThinNeo.TransactionType.InvocationTransaction;
-                var idata = new ThinNeo.InvokeTransData();
-                tran.extdata = idata;
-                idata.script = data;
-                idata.gas = 0;
-            }
-
-            //sign and broadcast
-            var signdata = ThinNeo.Helper.Sign(tran.GetMessage(), prikey);
-            tran.AddWitness(signdata, pubkey, address);
-            var trandata = tran.GetRawData();
-            var strtrandata = ThinNeo.Helper.Bytes2HexString(trandata);
-            byte[] postdata;
-            var url = Helper.MakeRpcUrlPost(nnc_1.api, "sendrawtransaction", out postdata, new MyJson.JsonNode_ValueString(strtrandata));
-            var result = await Helper.HttpPost(url, postdata);
-            return result;
+            subPrintLine(result);
         }
 
     }
