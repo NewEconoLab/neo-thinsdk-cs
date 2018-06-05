af903c737f098c8b9e337fdcea42f43c1f3838da
nuanbing <jinghui@wayne.edu>
2018-06-05 19:22:41 +0800

    新部署nnc，更换test_wif

--- smartContractDemo/tests/Config.cs
+++ smartContractDemo/tests/Config.cs
@@ -7,7 +7,7 @@ namespace smartContractDemo.tests
     class Config
     {
         public readonly static string superadminAddress = "ALjSnMZidJqd18iQaoCgFun6iqWRm2cVtj";
-        public  static string test_wif = "KwZih114osBp58RwpEn4ZAcEcCTLP6yMAhdikb6oPRxqgvWpcqF1";
+        public  static string test_wif = "KwF2QTMzDEmjEEfkbvkGTZTL8Ch17h1jvQjbLtFj4vHs7KfLrKir";
 
 
         public readonly static Hash160 dapp_sgas  = new Hash160("0xe52a08c20986332ad8dccf9ded38cc493878064a");//sgas 新合约地址
@@ -18,7 +18,8 @@ namespace smartContractDemo.tests
 
         public static readonly Hash160 dapp_multisign = new Hash160("0x4c0f57b61d997297560190b1e397fe6d58fce94a");  //应用合约多签验证测试
 
-        public readonly static Hash160 dapp_nnc = new Hash160("0xd9f8803a66acdd0c4218b4dca21a43c43e28b66c"); //nnc 合约代码
+        public readonly static Hash160 dapp_nnc = new Hash160("0x6102a56793c3dc6204eb70f988c46d8e7d25eaa1");
+                                                            //("0xd9f8803a66acdd0c4218b4dca21a43c43e28b66c"); //nnc 合约代码
 
         public const string id_GAS = "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7";
 
