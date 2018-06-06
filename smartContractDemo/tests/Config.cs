ad075cb9b7c3e8892dcfea97b395fad9d77d81ed
nuanbing <jinghui@wayne.edu>
2018-06-06 11:54:49 +0800

    合约地址更新

--- smartContractDemo/tests/Config.cs
+++ smartContractDemo/tests/Config.cs
@@ -10,15 +10,15 @@ namespace smartContractDemo.tests
         public  static string test_wif = "KwF2QTMzDEmjEEfkbvkGTZTL8Ch17h1jvQjbLtFj4vHs7KfLrKir";
 
 
-        public readonly static Hash160 dapp_sgas  = new Hash160("0xe52a08c20986332ad8dccf9ded38cc493878064a");//sgas 新合约地址
+        public readonly static Hash160 dapp_sgas  = new Hash160("0xed022d586745e3b0d3654ac86aa81ca88d68c0a2");//sgas 新合约地址
         public readonly static Hash160 dapp_coinpool = new Hash160("0x5d6b91ee7cde1f8bb1868d36d4bf134f6887d231");//coinpool 新合约地址
-        public static readonly Hash160 sc_nns = new Hash160("0xee46b4f6d2073381dc61ae74099ccf3a56e38dd1");//nns 跳板合约地址
-        public static readonly Hash160 domaincenterhash = new Hash160("0x5cbc9fbd11d7003ca3b47cbec1117caf6224f317");//nns 域名中心合约地址
+        public static readonly Hash160 sc_nns = new Hash160("0x537758fbe85505801faa7d7d7b75b37686ad7e2d");//nns 跳板合约地址
+        public static readonly Hash160 domaincenterhash = new Hash160("0x04f4ff5237391169b42b5c84a5313b43080a6990");//nns 域名中心合约地址
 
 
         public static readonly Hash160 dapp_multisign = new Hash160("0x4c0f57b61d997297560190b1e397fe6d58fce94a");  //应用合约多签验证测试
 
-        public readonly static Hash160 dapp_nnc = new Hash160("0x6102a56793c3dc6204eb70f988c46d8e7d25eaa1");
+        public readonly static Hash160 dapp_nnc = new Hash160("0x7054c7e606f7abd353838d2b03190d302f0db938");
                                                             //("0xd9f8803a66acdd0c4218b4dca21a43c43e28b66c"); //nnc 合约代码
 
         public const string id_GAS = "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7";
