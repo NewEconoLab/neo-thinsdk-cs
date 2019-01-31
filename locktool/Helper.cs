7c8743e0944d557427098715aabbd0613b27c9df
lights <lightsever@hotmail.com>
2019-01-31 23:53:26 +0800

    add locktool

---
+++ locktool/Helper.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+
+    public class Utxo
+    {
+        //txid[n] 是utxo的属性
+        public ThinNeo.Hash256 txid;
+        public int n;
+
+        //asset资产、addr 属于谁，value数额，这都是查出来的
+        public string addr;
+        public string asset;
+        public decimal value;
+        public Utxo(string _addr, ThinNeo.Hash256 _txid, string _asset, decimal _value, int _n)
+        {
+            this.addr = _addr;
+            this.txid = _txid;
+            this.asset = _asset;
+            this.value = _value;
+            this.n = _n;
+        }
+    }
+    public class Helper
+    {
+        public static string MakeRpcUrlPost(string url, string method, out byte[] data, params MyJson.IJsonNode[] _params)
+        {
+            //if (url.Last() != '/')
+            //    url = url + "/";
+            var json = new MyJson.JsonNode_Object();
+            json["id"] = new MyJson.JsonNode_ValueNumber(1);
+            json["jsonrpc"] = new MyJson.JsonNode_ValueString("2.0");
+            json["method"] = new MyJson.JsonNode_ValueString(method);
+            StringBuilder sb = new StringBuilder();
+            var array = new MyJson.JsonNode_Array();
+            for (var i = 0; i < _params.Length; i++)
+            {
+
+                array.Add(_params[i]);
+            }
+            json["params"] = array;
+            data = System.Text.Encoding.UTF8.GetBytes(json.ToString());
+            return url;
+        }
+        public static string MakeRpcUrl(string url, string method, params MyJson.IJsonNode[] _params)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (url.Last() != '/')
+                url = url + "/";
+
+            sb.Append(url + "?jsonrpc=2.0&id=1&method=" + method + "&params=[");
+            for (var i = 0; i < _params.Length; i++)
+            {
+                _params[i].ConvertToString(sb);
+                if (i != _params.Length - 1)
+                    sb.Append(",");
+            }
+            sb.Append("]");
+            return sb.ToString();
+        }
+
+
+
+        public static async Task<List<Utxo>> GetUtxosToPay(string api, string _addr, string assetid, double number)
+        {
+            MyJson.JsonNode_Object response = (MyJson.JsonNode_Object)MyJson.Parse(await Helper.HttpGet(api + "?method=getutxostopay&id=1&params=["
+                + "'" + _addr + "','" + assetid + "'," + number + "]"));
+            MyJson.JsonNode_Array resJA = (MyJson.JsonNode_Array)response["result"];
+            List<Utxo> list = new List<Utxo>();
+            foreach (MyJson.JsonNode_Object j in resJA)
+            {
+                Utxo utxo = new Utxo(j["addr"].ToString(), new ThinNeo.Hash256(j["txid"].ToString()), j["asset"].ToString(), decimal.Parse(j["value"].ToString()), int.Parse(j["n"].ToString()));
+                list.Add(utxo);
+            }
+
+            return list;
+        }
+
+        //获取地址的utxo来得出地址的资产  
+        public static async Task<Dictionary<string, List<Utxo>>> GetBalanceByAddress(string api, string _addr)
+        {
+            MyJson.JsonNode_Object response = (MyJson.JsonNode_Object)MyJson.Parse(await Helper.HttpGet(api + "?method=getutxo&id=1&params=['" + _addr + "']"));
+            MyJson.JsonNode_Array resJA = (MyJson.JsonNode_Array)response["result"];
+            Dictionary<string, List<Utxo>> _dir = new Dictionary<string, List<Utxo>>();
+            foreach (MyJson.JsonNode_Object j in resJA)
+            {
+                Utxo utxo = new Utxo(j["addr"].ToString(), new ThinNeo.Hash256(j["txid"].ToString()), j["asset"].ToString(), decimal.Parse(j["value"].ToString()), int.Parse(j["n"].ToString()));
+                if (_dir.ContainsKey(j["asset"].ToString()))
+                {
+                    _dir[j["asset"].ToString()].Add(utxo);
+                }
+                else
+                {
+                    List<Utxo> l = new List<Utxo>();
+                    l.Add(utxo);
+                    _dir[j["asset"].ToString()] = l;
+                }
+
+            }
+            return _dir;
+        }
+        public static ThinNeo.Transaction makeTran(List<Utxo> utxos, string targetaddr, ThinNeo.Hash256 assetid, decimal sendcount, decimal extgas = 0, List<Utxo> utxos_ext = null, string extaddr = null)
+        {
+            var tran = new ThinNeo.Transaction();
+            tran.type = ThinNeo.TransactionType.ContractTransaction;
+            if (extgas >= 1)
+            {
+                tran.version = 1;//0 or 1
+            }
+            else
+            {
+                tran.version = 0;//0 or 1
+            }
+            tran.extdata = null;
+
+            tran.attributes = new ThinNeo.Attribute[0];
+            var scraddr = "";
+            decimal count = decimal.Zero;
+            List<ThinNeo.TransactionInput> list_inputs = new List<ThinNeo.TransactionInput>();
+
+            if (utxos != null)
+            {
+                utxos.Sort((a, b) =>
+                {
+                    if (a.value > b.value)
+                        return 1;
+                    else if (a.value < b.value)
+                        return -1;
+                    else
+                        return 0;
+                });
+                for (var i = 0; i < utxos.Count; i++)
+                {
+                    ThinNeo.TransactionInput input = new ThinNeo.TransactionInput();
+                    input.hash = utxos[i].txid;
+                    input.index = (ushort)utxos[i].n;
+                    list_inputs.Add(input);
+                    count += utxos[i].value;
+                    scraddr = utxos[i].addr;
+                    if (count >= sendcount)
+                    {
+                        break;
+                    }
+                }
+            }
+            decimal count_ext = decimal.Zero;
+            if (utxos_ext != null)
+            {
+                //手续费
+                ThinNeo.TransactionInput input = new ThinNeo.TransactionInput();
+                input.hash = utxos_ext[0].txid;
+                input.index = (ushort)utxos_ext[0].n;
+                count_ext = utxos_ext[0].value;
+                list_inputs.Add(input);
+            }
+
+            tran.inputs = list_inputs.ToArray();
+            if (count >= sendcount)//输入大于等于输出
+            {
+                List<ThinNeo.TransactionOutput> list_outputs = new List<ThinNeo.TransactionOutput>();
+                //输出
+                if (sendcount > decimal.Zero && targetaddr != null)
+                {
+                    ThinNeo.TransactionOutput output = new ThinNeo.TransactionOutput();
+                    output.assetId = assetid;
+                    output.value = sendcount;
+                    output.toAddress = ThinNeo.Helper.GetPublicKeyHashFromAddress(targetaddr);
+                    list_outputs.Add(output);
+                }
+                var change = count - sendcount - extgas;
+                decimal extchange = decimal.Zero;
+                //找零
+                if (utxos_ext != null)
+                {
+                    change = count - sendcount;
+                    extchange = count_ext - extgas;
+                }
+                else
+                {
+                    change = count - sendcount - extgas;
+                }
+                if (change > decimal.Zero)
+                {
+                    ThinNeo.TransactionOutput outputchange = new ThinNeo.TransactionOutput();
+                    outputchange.toAddress = ThinNeo.Helper.GetPublicKeyHashFromAddress(scraddr);
+                    outputchange.value = change;
+                    outputchange.assetId = assetid;
+                    list_outputs.Add(outputchange);
+                }
+                if (extchange > decimal.Zero)
+                {
+                    ThinNeo.TransactionOutput outputchange = new ThinNeo.TransactionOutput();
+                    outputchange.toAddress = ThinNeo.Helper.GetPublicKeyHashFromAddress(extaddr);
+                    outputchange.value = extchange;
+                    outputchange.assetId = assetid;
+                    list_outputs.Add(outputchange);
+                }
+                tran.outputs = list_outputs.ToArray();
+            }
+            else
+            {
+                throw new Exception("no enough money.");
+            }
+            return tran;
+        }
+
+        /// <summary>
+        /// 同步get请求
+        /// </summary>
+        /// <param name="url">链接地址</param>    
+        /// <param name="formData">写在header中的键值对</param>
+        /// <returns></returns>
+
+        public static async Task<string> HttpGet(string url)
+        {
+            WebClient wc = new WebClient();
+            wc.Headers["content-type"] = "text/plain;charset=UTF-8";
+
+            return await wc.DownloadStringTaskAsync(url);
+        }
+        public static async Task<string> HttpPost(string url, byte[] data)
+        {
+            WebClient wc = new WebClient();
+            wc.Headers["content-type"] = "text/plain;charset=UTF-8";
+            byte[] retdata = await wc.UploadDataTaskAsync(url, "POST", data);
+            return System.Text.Encoding.UTF8.GetString(retdata);
+        }
+    }
+}
--- locktool/MainWindow.xaml.cs
+++ locktool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,12 +25,19 @@ namespace locktool
         {
             InitializeComponent();
         }
+        string api = "https://api.nel.group/api/testnet";
+
+        const string id_GAS = "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7";
         const string lockscript = "56c56b6c766b00527ac46c766b51527ac46c766b52527ac4616168184e656f2e426c6f636b636861696e2e4765744865696768746168184e656f2e426c6f636b636861696e2e4765744865616465726c766b53527ac46c766b00c36c766b53c36168174e656f2e4865616465722e47657454696d657374616d70a06c766b54527ac46c766b54c3640e00006c766b55527ac4621a006c766b51c36c766b52c3617cac6c766b55527ac46203006c766b55c3616c7566";
+        // script hash  = d3cce84d0800172d09c88ccad61130611bd047a4
         private static readonly DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         public static uint ToTimestamp(DateTime time)
         {
             return (uint)(time.ToUniversalTime() - unixEpoch).TotalSeconds;
         }
+        public byte[] script_LockContract;
+        public byte[] scripthash_LockContract;
+        public string address_LockContract;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //显示lock合约地址
@@ -46,6 +54,8 @@ namespace locktool
             var addr = ThinNeo.Helper.GetAddressFromPublicKey(pubkey);
             label_addr.Text = addr;
 
+            //默认提取地址就是私钥地址
+            txt_get_addr.Text = addr;
             //显示日期
             var date = datepicker.SelectedDate;
             if (date == null)
@@ -56,7 +66,7 @@ namespace locktool
                 date.Value.Minute,
                 date.Value.Second);
             label_time.Text = datemix.ToLongDateString() + " " + datemix.ToLongTimeString();
-            label_timeutc.Text = datemix.ToLongDateString() + " " + datemix.ToLongTimeString();
+            label_timeutc.Text = datemix.ToUniversalTime().ToLongDateString() + " " + datemix.ToUniversalTime().ToLongTimeString();
 
 
             //生成脚本
@@ -70,16 +80,237 @@ namespace locktool
                 sb.EmitPushNumber(timestamp);//sb.EmitPush(timestamp);
 
                 //// Lock 2.0 in mainnet tx:4e84015258880ced0387f34842b1d96f605b9cc78b308e1f0d876933c2c9134b
-                sb.EmitAppCall(hash);
+                sb.EmitAppCall(hash); // script hash  = d3cce84d0800172d09c88ccad61130611bd047a4
                 //return Contract.Create(new[] { ContractParameterType.Signature }, sb.ToArray());
                 script = sb.ToArray();
             }
 
-            
+
             var callscripthash = ThinNeo.Helper.GetScriptHashFromScript(script);
             var contractaddr = ThinNeo.Helper.GetAddressFromScriptHash(callscripthash);
             this.txt_contract.Text = ThinNeo.Helper.Bytes2HexString(script);
             this.txt_addrout.Text = contractaddr;
+
+
+            //from addr
+            this.txt_get_srcaddr.Text = contractaddr;
+
+            address_LockContract = contractaddr;
+            script_LockContract = script;
+            scripthash_LockContract = callscripthash;
+        }
+        class Asset
+        {
+            public bool isnep5;
+            public string assetID;
+            public double balance;
+            public string name;
+            public int decimals = 0;
+            public override string ToString()
+            {
+                return (isnep5 ? "<NEP5>" : "<UTXO>") + name + "=" + balance;
+            }
+        }
+        bool hasGas;
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            listAsset.Items.Clear();
+            {
+                //fill utxo balance;
+                var url = Demo.Helper.MakeRpcUrlPost(this.api, "getbalance", out byte[] data, new MyJson.JsonNode_ValueString(address_LockContract));
+                var json = await Demo.Helper.HttpPost(url, data);
+                var jsonb = Newtonsoft.Json.Linq.JObject.Parse(json);
+                var result = jsonb["result"] as JArray;
+                hasGas = false;
+                if (result != null)
+                {
+                    foreach (JObject r in result)
+                    {
+                        var asset = new Asset();
+                        asset.isnep5 = false;
+                        asset.assetID = (string)r["asset"];
+                        asset.balance = (double)r["balance"];
+                        foreach (JObject nameitem in r["name"] as JArray)
+                        {
+                            asset.name = nameitem["name"].ToString();
+                        }
+                        if (asset.assetID == id_GAS)
+                        {
+                            asset.name = "::GAS";
+                            hasGas = true;
+                        }
+                        this.listAsset.Items.Add(asset);
+                        var url2 = Demo.Helper.MakeRpcUrlPost(this.api, "getasset", out byte[] data2, new MyJson.JsonNode_ValueString(asset.assetID));
+                        var json2 = await Demo.Helper.HttpPost(url2, data2);
+                        var jsonb2 = Newtonsoft.Json.Linq.JObject.Parse(json2);
+
+                        asset.decimals = (int)jsonb2["result"][0]["precision"];
+                    }
+                }
+            }
+            {//fill nep5 balance.
+                var url2 = Demo.Helper.MakeRpcUrlPost(this.api, "getallnep5assetofaddress", out byte[] data2, new MyJson.JsonNode_ValueString(address_LockContract));
+                var json = await Demo.Helper.HttpPost(url2, data2);
+                var jsonb = Newtonsoft.Json.Linq.JObject.Parse(json);
+                var result = (jsonb["result"] as JArray);
+                if (result != null)
+                {
+                    foreach (JObject item in result)
+                    {
+                        var asset = new Asset();
+                        asset.isnep5 = true;
+                        asset.assetID = (string)item["assetid"];
+                        var urlsub = Demo.Helper.MakeRpcUrlPost(this.api, "getnep5asset", out byte[] datasub, new MyJson.JsonNode_ValueString(asset.assetID));
+                        var jsonsub = await Demo.Helper.HttpPost(urlsub, datasub);
+                        var jsonsubb = Newtonsoft.Json.Linq.JObject.Parse(jsonsub);
+                        asset.name = (string)jsonsubb["result"][0]["name"];
+                        asset.decimals = (int)jsonsubb["result"][0]["decimals"];
+
+                        var urlb = Demo.Helper.MakeRpcUrlPost(this.api, "getnep5balanceofaddress", out byte[] databsub, new MyJson.JsonNode_ValueString(asset.assetID), new MyJson.JsonNode_ValueString(address_LockContract));
+                        var jsonbsub = await Demo.Helper.HttpPost(urlb, databsub);
+                        var jsonbsubb = Newtonsoft.Json.Linq.JObject.Parse(jsonbsub);
+                        asset.balance = (double)jsonbsubb["result"][0]["nep5balance"];
+                        this.listAsset.Items.Add(asset);
+
+                    }
+                }
+            }
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            byte[] rawdata = await MakeScript(true);
+
+            if (rawdata == null)
+                return;
+
+            var txt = ThinNeo.Helper.Bytes2HexString(rawdata);
+            this.txt_get_result.Text = txt;
+
+            //and broadcast;
+            {
+                //ThinNeo.Transaction testde = new ThinNeo.Transaction();
+                //testde.Deserialize(new System.IO.MemoryStream(tran.GetRawData()));
+
+                byte[] postdata;
+                var url = Demo.Helper.MakeRpcUrlPost(api, "sendrawtransaction", out postdata, new MyJson.JsonNode_ValueString(txt));
+                var resultstr = await Demo.Helper.HttpPost(url, postdata);
+
+                this.txt_bc_result.Text = resultstr;
+                MessageBox.Show("boardcast that=" + resultstr);
+            }
+        }
+        public async Task<byte[]> MakeScript(bool sign)
+        {
+            if (hasGas == false)
+            {
+                MessageBox.Show("you do not have gas for sendraw.");
+                return null;
+
+            }
+            var asset = listAsset.SelectedItem as Asset;
+            if (asset == null || asset.isnep5 == false)
+            {
+                MessageBox.Show("this is not a nep5");
+                return null;
+            }
+            var targetaddr = txt_get_addr.Text;
+            var targetbalance = double.Parse(txt_get_balance.Text);
+            for (var i = 0; i < asset.decimals; i++)
+            {
+                targetbalance *= 10;
+            }
+
+            var payaddr = label_addr.Text;
+            var paygas = 0.001;
+            var utxos = await Demo.Helper.GetUtxosToPay(api, payaddr, id_GAS, paygas);
+            if (sign)
+            {
+                var paywif = txt_wifGetter.Text;
+                var prikey = ThinNeo.Helper.GetPrivateKeyFromWIF(paywif);
+                var pubkey = ThinNeo.Helper.GetPublicKeyFromPrivateKey(prikey);
+                var address = ThinNeo.Helper.GetAddressFromPublicKey(pubkey);
+                if (address != payaddr)
+                {
+                    MessageBox.Show("error wif for pubkey in lock contract.");
+                    return null;
+                }
+            }
+            //MakeTran
+            ThinNeo.Transaction tran = null;
+            {
+
+                byte[] script = null;
+                using (var sb = new ThinNeo.ScriptBuilder())
+                {
+                    var array = new MyJson.JsonNode_Array();
+                    array.AddArrayValue("(addr)" + address_LockContract);//from
+                    array.AddArrayValue("(addr)" + targetaddr);//to
+                    array.AddArrayValue("(int)" + (ulong)targetbalance);//value
+                    sb.EmitParamJson(array);//参数倒序入
+                    sb.EmitParamJson(new MyJson.JsonNode_ValueString("(str)transfer"));//参数倒序入
+                    ThinNeo.Hash160 shash = new ThinNeo.Hash160(asset.assetID);
+                    sb.EmitAppCall(shash);//nep5脚本
+                    script = sb.ToArray();
+                }
+
+                tran = Demo.Helper.makeTran(null, payaddr, new ThinNeo.Hash256(id_GAS), 0, (decimal)0.001, utxos, payaddr);
+                tran.type = ThinNeo.TransactionType.InvocationTransaction;
+                var idata = new ThinNeo.InvokeTransData();
+                tran.extdata = idata;
+                idata.script = script;
+                tran.attributes = new ThinNeo.Attribute[1];
+                tran.attributes[0] = new ThinNeo.Attribute();
+                tran.attributes[0].usage = ThinNeo.TransactionAttributeUsage.Script;
+                tran.attributes[0].data = scripthash_LockContract;
+            }
+
+            if (sign)
+            {
+                var paywif = txt_wifGetter.Text;
+                var prikey = ThinNeo.Helper.GetPrivateKeyFromWIF(paywif);
+                var pubkey = ThinNeo.Helper.GetPublicKeyFromPrivateKey(prikey);
+                var address = ThinNeo.Helper.GetAddressFromPublicKey(pubkey);
+
+                ////sign 
+                var signdata = ThinNeo.Helper.Sign(tran.GetMessage(), prikey);
+                tran.AddWitness(signdata, pubkey, address);
+
+                var sb2 = new ThinNeo.ScriptBuilder();
+                sb2.EmitPushBytes(signdata);
+
+                var iscript = sb2.ToArray();
+                tran.AddWitnessScript(script_LockContract, iscript);
+                return tran.GetRawData();
+
+            }
+            else
+            {
+                return tran.GetMessage();
+            }
+
+        }
+
+
+        private async void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            byte[] rawdata = await MakeScript(true);
+            if (rawdata == null)
+                return;
+
+            var txt = ThinNeo.Helper.Bytes2HexString(rawdata);
+            this.txt_get_result.Text = txt;
+
+        }
+
+        private async void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            byte[] rawdata = await MakeScript(false);
+            if (rawdata == null)
+                return;
+
+            var txt = ThinNeo.Helper.Bytes2HexString(rawdata);
+            this.txt_get_result.Text = txt;
         }
     }
 }
